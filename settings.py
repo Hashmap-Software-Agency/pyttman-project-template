ed3ea4a96edcf97808afa7a9d41b597013aad95e
Simon Olofsson <36161882+dotchetter@users.noreply.github.com>
2022-04-06 23:54:05 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = {
     # app isn't simply ignoring them by keeping quiet.
     # In the log files of your Pyttman app, you can track down the error
     # by searching for the Error UUID written in chat when the error occurred.
-    FATAL_EXCEPTION_AUTO_REPLY = "I'm sorry, something went wrong. Try again" \
+    "FATAL_EXCEPTION_AUTO_REPLY" = "I'm sorry, something went wrong. Try again" \
                                  " in a few moments."
 }
 
